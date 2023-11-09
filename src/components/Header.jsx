77a83571a3db059f2fd5efca75853ec87b1d8878
thelight0804 <thelight0804@gmail.com>
2023-11-10 00:00:34 +0900

    Add: 로고 이미지와 메뉴 위치 이동
    - 컨테이너를 flex로 적용
    - 로고 이미지 크기 고정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,22 @@
 import React from "react";
+
 import styles from './header.module.scss'
+import OpenMenu from "./OpenMenu";
 
 function Header(){
   return (
-    <div>
-      <img src={"public/img/cahlp-dark.png"} width="50%"/>
-      <button className={styles.toggleButton}>
-        <span className={styles.dot}></span>
-        <span className={`${styles.dot} ${styles.dotCenter}`}></span>
-        <span className={styles.dot}></span>
-      </button>
-    </div>
+      <div className={styles.container}>
+          <img
+              src={"public/img/cahlp-dark.png"}
+              className={styles.logo}
+              onClick={console.log("clicked logo")}
+          />
+          <button className={styles.toggleButton}>
+              <span className={styles.dot}></span>
+              <span className={`${styles.dot} ${styles.dotCenter}`}></span>
+              <span className={styles.dot}></span>
+          </button>
+      </div>
   );
 }
 
