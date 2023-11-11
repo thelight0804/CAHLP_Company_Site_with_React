4dada145ed014ad025e40be7698f9b5fea4aea33
thelight0804 <thelight0804@gmail.com>
2023-11-11 14:43:55 +0900

    Add: 네비게이션 메뉴 추가
    햄버거 버튼을 클릭하면 다른 페이지로 넘어갈 수 있는 네비게이션 메뉴가 나타납니다
    - cahlp-dart 이미지 크기 조정
    - 메뉴가 나타남에 따라 동적으로 색깔 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,51 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 
 import styles from './header.module.scss'
-import Main from "../pages/Main";
+import { useNavigate } from "react-router-dom";
 
 function Header(){
-  let navigate = useNavigate();
+  var [isModalOpen, setIsModalOpen] = React.useState(true); // modal open 여부
+  var navigate = useNavigate(); // react router의 navigate 함수
 
   return (
-      <div className={styles.container}>
-          <img
-              src={"public/img/cahlp-dark.png"}
+      <>
+        <div className={`${styles.header} ${isModalOpen && styles.modalOpen}`}>
+          {isModalOpen ? (
+            <img src={"public/img/cahlp-white.png"}
               className={styles.logo}
-              onClick={()=>{navigate("/");}}
-          />
-          <button 
-            className={styles.toggleButton}
-            onClick={()=>{console.log('click!')}}
+              onClick={() => { navigate("/"); }} />
+          ) : (
+            <img src={"public/img/cahlp-dark.png"}
+              className={styles.logo}
+              onClick={() => { navigate("/"); }} />
+          )}
+          <button
+              className={styles.toggleButton}
+              onClick={() => {isModalOpen ? setIsModalOpen(false) : setIsModalOpen(true);}}
           >
               <span className={styles.dot}></span>
               <span className={`${styles.dot} ${styles.dotCenter}`}></span>
               <span className={styles.dot}></span>
           </button>
-      </div>
+        </div>
+        {isModalOpen && <NavMenu isModalOpen={isModalOpen}/>}
+      </>
+  );
+}
+
+/**
+ * 메뉴 버튼 클릭 시 다른 경로로 안내하는 모달 컴포넌트
+ */
+function NavMenu(props){
+  return(
+    <div className={`${styles.nav} ${props.isModalOpen && styles.modalOpen}`}>
+      <button className={styles.navButton}
+        onClick={console.log("우미 브랜드 (준비중)")}
+      >우미 브랜드 (준비중)</button>
+      <button className={styles.navButton}
+        onClick={console.log("E-Service")}
+      >E-Service</button>
+    </div>
   );
 }
 
