a647de617decbceb1196b55e64789cc5b5124543
thelight0804 <thelight0804@gmail.com>
2023-11-14 13:34:34 +0900

    Add: 메인 화면 비디오
    - 비디오 무한 재생
    - 화면 채우기
    - 텍스트를 비디오 위에 위치
    - 텍스트 중앙 정렬

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -4,16 +4,21 @@ import styles from './main.module.scss'
 
 function Main(){
   return (
-    <div className="container">
-      <div className="header">
+    <div className={styles.container}>
+      <div className={styles.header}>
         <video muted autoPlay loop>
           <source src={"./../../public/video/via.mp4"} type="video/mp4" />
         </video>
+        <div className={styles.mainText}>
+          <h2>" 누구나 마음 편히 이색반려동물을 키울수 있는 그날까지."</h2>
+          <h3>누구나, 언제, 어디서든, 관상어, 육상생물들도 안전하고, <br/>편리하게 키울수 있는 세상을 만들고 싶습니다.</h3>
+        </div>
       </div>
-      <div className="content">
+      <div className={styles.content}>
+        <h2>"우리는 이런 고민을 했습니다."</h2>
 
       </div>
-      <div className="footer">
+      <div className={styles.footer}>
 
       </div>
     </div>
